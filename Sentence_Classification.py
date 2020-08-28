fa5dce63c73f906ec6fbdb8368ab985195a7340d
nguyenvantan0125 <44522645+nguyenvantan0125@users.noreply.github.com>
2020-08-28 22:55:39 +0700

    Update Sentence_Classification.py

--- Sentence_Classification.py
+++ Sentence_Classification.py
@@ -1,8 +1,7 @@
     # Import module was created
-import textclassification as tx
-
+import module as tx
     # Read data from CSV - đọc data từ file csv
-df = tx.readcsv("texttrain.csv")
+df = tx.readcsv("E:\\Python\\texttrain.csv")
 df.rename(columns={0:'feature',1:'label'},inplace=True)
 
     # create corpus - tạo bộ văn bản
@@ -11,43 +10,20 @@ corpus = df['feature'].values.tolist()
     # create label - tạo label 
 y = df['label'].values.tolist()
     
-    # Entry sentence relate to greeting or asking weather(require Vietnamese)
+    # Entry sentence relate to greeting or asking weather(Vietnamese requirement)
     # Nhập nhập những câu về chào hỏi hoặc hỏi về thời tiết (yêu cầu nhập tiếng việt)
-print("Entry sentence relate to greeting OR asking weather(require Vietnamese)")
-print("Nhập nhập những câu về chào hỏi HOẶC hỏi về thời tiết (yêu cầu nhập tiếng việt)")
+print("\nEntry sentence relate to greeting OR asking weather(Vietnamese requirement)")
+print("\nNhập nhập những câu về chào hỏi HOẶC hỏi về thời tiết (yêu cầu nhập tiếng việt)")
 test_data = [str(input('entry: '))]
 
-    # tokenizer Vietnamese - thực hiện tách từ 
-corpus = tx.vitoken(corpus)
-test_data = tx.vitoken(test_data)
+    # Model SVC
+model_SVC = tx.SVC_linear(corpus,y,test_data)
+model_SVC.processing()
 
-    # onehotvector - Mã hóa bộ văn bản
-vect = tx.vectorizer.countvector(corpus)
-bow = tx.vectorizer.bag_of_words(vect,corpus)
+    # Model Navie Bayes 
+model_NB = tx.NavieBayes(corpus,y,test_data)
+model_NB.processing()
 
-    # create training set 
-X_train,X_test= tx.preprocessing.processing(vect,corpus,test_data)
-
-
-# SVC_linear training AND show result
-tx.models_selections.model_SVC_linear (X_train, y , X_test)
-
-
-# SVC_rbf training AND show result
-tx.models_selections.model_SVC_rbf(X_train, y , X_test)
-
-
-# Naive Bayers AND show result
-tx.models_selections.pipeline_NB(corpus,y,test_data)
-
-
-
-
-
-
-
-
-
-
-
-
+    # Compute Cosine Similarity
+con_sim = tx.Cosine_Sim(corpus,y,test_data)
+con_sim.processing()
