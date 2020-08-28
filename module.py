4d2fef658d405c89f0dc7fea74a49c7cb60a5c32
nguyenvantan0125 <44522645+nguyenvantan0125@users.noreply.github.com>
2020-08-28 22:56:48 +0700

    Update module.py

--- module.py
+++ module.py
@@ -1,104 +1,98 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 22 18:48:03 2020
-
-@author: nguye
-"""
 from pyvi import ViTokenizer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
-from sklearn.feature_extraction.text import  TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import svm
 from sklearn.pipeline import Pipeline
 import pandas 
-
+    
+    # read file CSV
 def readcsv(filename):
-            a = pandas.read_csv(filename,header = None)
-            return a 
-def vitoken(corpus):       
-            for idx, txt in enumerate(corpus):
-                corpus[idx] = ViTokenizer.tokenize(txt) 
-            return corpus
-            
-#%%
-class vectorizer():
-    def countvector(corpus):
-        vect = CountVectorizer(token_pattern=u"(?u)\\b\\w+\\b")
-        vect.fit(corpus)
-        return vect
-    def countvector_array(vect,corpus):
-        X = vect.transform(corpus).toarray()
-        return X
-    def bag_of_words(vect,cotpus):
-        bow = vect.vocabulary_
-        return bow
-    def tfidf(corpus):
-        vect_tfidf = TfidfVectorizer(token_pattern=u"(?u)\\b\\w+\\b")
-        vect_tfidf.fit(corpus)
-        return vect_tfidf
-    def tfidf_array(vect,corpus):
-        X = vect.transform(corpus).toarray()
-        return X
-#%%
-class preprocessing(vectorizer):
-        
-    def encoder (y):
-            le = LabelEncoder()
-            y = le.fit_transform(y)
-            return y
-    @classmethod
-    def processing(cls,vectorizer,corpus,test): # corpus và test đã mã hóa, 
-        X_train = super().countvector_array(vectorizer,corpus)
-        X_test = super().countvector_array(vectorizer,test)
-        yield X_train
-        yield X_test
-#%%
-class models_selections(object):
-    def model_SVC_linear (X,y,test_data):
-        model = svm.SVC( probability = True,kernel='linear')
-        model.fit(X,y)
-        #predict
-        predicted = model.predict(test_data)
-        print('\n\t Dung SVC-linear de tinh:')
-        print ("\nPredicted Value:", predicted)
-        
-    def model_SVC_rbf (X,y,test_data):
-        model = svm.SVC( probability = True, kernel='rbf') 
-        model.fit(X,y)
-        #predict
-        predicted = model.predict(test_data)
-        print('\n\t Dung SVC_rbf de tinh:')
-        print ("\nPredicted Value:", predicted)
-        
-    def pipeline_NB(corpus,y,test):
-        pipe_line = Pipeline([
-            ("vect", CountVectorizer()),#bag-of-words
-            # ("tfidf", TfidfTransformer()),#tf-idf
-            ("clf", MultinomialNB()) #model
-            ])
-        pipe_line.fit(corpus,y)
-        predicted = pipe_line.predict(test)
-        print('\n\t Dung Naive Bayer de tinh:')
-        print ("\nPredicted Value:", predicted)
-        
-        
-#%%
+    a = pandas.read_csv(filename,header = None)
+    return a 
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+    # Tokenizer - tách từ tiếng việt
+class tokenizer():
+    def __init__(self,corpus):
+        self.corpus = corpus
+    def vi_tokenizer(self):
+        for idx, txt in enumerate(self.corpus):
+            self.corpus[idx] = ViTokenizer.tokenize(txt)
+        return self.corpus
+
+    
+class SVC_linear():
+    """
+    1. Khởi tạo và thực hiện tách từ tiếng việt
+    2. Tạo pipeline từ scikit-learn 
+        2.1. Encoder bằng CountVectorizer
+        2.2. Chọn model SVC-learn
+    3. Fit và Predict 
+    4. Print kết quả 
+    """
+   
+    def __init__(self, raw_corpus,y, sentence):
+        self.corpus = tokenizer(raw_corpus).vi_tokenizer()
+        self.y = y
+        self.sentence = tokenizer(sentence).vi_tokenizer()     
+    def processing(self):
+        pipe_line = Pipeline([
+            ("vect", CountVectorizer(token_pattern=u"(?u)\\b\\w+\\b")),#bag-of-words
+            ("clf", svm.SVC(probability = True, kernel='linear')) #model
+            ])
+        pipe_line.fit(self.corpus,self.y)
+        predicted = pipe_line.predict(self.sentence)
+        predict_proba = pipe_line.predict_proba(self.sentence)
+        print('\n\t SVC-linear:')
+        print ("\nPredicted Value:", *predicted)
+        print ("\nPredict_Proba: ", max(*predict_proba))
+
+class NavieBayes():
+    """
+    1. Khởi tạo và thực hiện tách từ tiếng việt
+    2. Tạo pipeline từ scikit-learn 
+        2.1. Encoder bằng CountVectorizer
+        2.2. Chọn model Navie Bayes
+    3. Fit và Predict 
+    4. Print kết quả 
+    """
+    def __init__(self, raw_corpus,y, sentence):
+        self.corpus = tokenizer(raw_corpus).vi_tokenizer()
+        self.y = y
+        self.sentence = tokenizer(sentence).vi_tokenizer()     
+    def processing(self):
+        pipe_line = Pipeline([
+            ("vect", CountVectorizer(token_pattern=u"(?u)\\b\\w+\\b")),
+            ("clf", MultinomialNB())
+            ])
+        pipe_line.fit(self.corpus,self.y)
+        predicted = pipe_line.predict(self.sentence)
+        predict_proba = pipe_line.predict_proba(self.sentence)
+        print('\n\t Naive Bayer:')
+        print ("\nPredicted Value:", *predicted)
+        print ("\nPredict_Proba: ", max(*predict_proba))
+
+class Cosine_Sim():
+    def __init__(self,raw_corpus, y, sentence):
+        self.corpus = tokenizer(raw_corpus).vi_tokenizer()
+        self.y = y
+        self.sentence = tokenizer(sentence).vi_tokenizer()  
+    def processing(self):
+        tfidf = TfidfVectorizer(token_pattern=u"(?u)\\b\\w+\\b")
+        X_train = tfidf.fit_transform(self.corpus).toarray()
+        X_test = tfidf.transform(self.sentence).toarray()
+        result = []
+        for idx in range(len(X_train)):
+            x = X_train[idx].reshape(1,len(X_train[idx]))
+            a =  cosine_similarity(x,X_test)
+            result.append(a[0])
+        m = max(result)
+        for i, j in enumerate(result): 
+            if j == m:
+                idx = i
+                break
+    # show result
+        print('\n\t Consine Similarity:')
+        print('\nPredict Values: ',self.y[idx])
         
